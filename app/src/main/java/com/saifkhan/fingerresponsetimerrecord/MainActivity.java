6d1fbfa9af3ac85281198cc4cd896fa8999d9c76
Saif Ahammod Khan <saif.ahammod@northsouth.edu>
2021-02-06 01:20:40 +0600

    Main XML done

--- app/src/main/java/com/saifkhan/fingerresponsetimerrecord/MainActivity.java
+++ app/src/main/java/com/saifkhan/fingerresponsetimerrecord/MainActivity.java
@@ -3,9 +3,12 @@ package com.saifkhan.fingerresponsetimerrecord;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    String age,gender;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +18,23 @@ public class MainActivity extends AppCompatActivity {
         * Then I have to Take the time value in a array...
         * then Store The Data in Firebase...
         * */
+
+
+    }
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radio_male_main:
+                if (checked)
+                    gender = "M";
+                break;
+            case R.id.radio_female_main:
+                if (checked)
+                    gender = "F";
+                break;
+        }
     }
 }
